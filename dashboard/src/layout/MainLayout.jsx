6f929b13f1482fa721a73b93e76f6d235bc3aa25
shahariaz <shahariaz.info@gmail.com>
2024-05-18 02:13:19 +0600

    added new future in admin dashborad

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -7,8 +7,8 @@ const MainLayout = () => {
   const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className="bg-[#cdcae9] w-full min-h-screen">
-      <Header />
-      <Sidebar />
+      <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
         <Outlet />
       </div>
--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import { getNav } from "../navigation/index";
 import { LuLogOut } from "react-icons/lu";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
   const { pathname } = useLocation();
   const [allNav, setAllNav] = useState([]);
 
@@ -14,10 +14,17 @@ const Sidebar = () => {
 
   return (
     <div>
-      <div></div>
+      <div
+        onClick={() => setShowSidebar(false)}
+        className={`fixed duration-200 ${
+          !showSidebar ? "invisible" : "visible"
+        } w-screen h-screen bg-[#144c7e89] top-0 z-10`}
+      ></div>
 
       <div
-        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
+          showSidebar ? "left-0" : "-left-[260px] lg:left-0"
+        }`}
       >
         <div className="h-[70px] flex justify-center items-center">
           <Link to="/" className="w-[180px] h-[50px]">
